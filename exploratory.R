b1059334709b14e5d3952cfb49d7b579c2bc553d
Cody Crunkilton <codycrunkilton@gmail.com>
2019-05-25 20:17:32 -0400

    shrunk network to 3+ connections, ID policy judges fairly well now

--- exploratory.R
+++ exploratory.R
@@ -1,7 +1,10 @@
 source("C:/Users/Cody/Dropbox/functions.R")
 setwd("C:/Users/Cody/Dropbox/debate_r")
+setwd("/Users/codycrunkilton/Dropbox/debate_r")
 
 library(ggraph)
+library(tidygraph)
+library(tidyverse)
 
 # looking at stuff --------------------------------------------------------
 
@@ -68,7 +71,7 @@ s %>%
 s %>% 
   mutate(side = case_when(side == 1 ~ "Aff",
                           side == 2 ~ "Neg")) %>% 
-  filter(side %>% is.na == F) %>% 
+  filter(side %>% is.na == F & judge != "NA NA") %>% 
   group_by(side) %>% 
   summarise(win = mean(win, na.rm = T),
             rounds = n_distinct(panel))
@@ -80,7 +83,23 @@ season %>%
   select(j_first, j_last, round, win, tournname) %>% 
   print(n = Inf)
 
+## most times judged by someone
 
+s %>% 
+  filter(judge != "NA NA") %>% 
+  group_by(team, judge) %>% 
+  summarise(ct = n(),
+            winpct = mean(win)) %>% 
+  arrange(ct %>% desc) %>% 
+  print(n = 30)
+
+
+s %>% 
+  filter(judge == "David Cram Helwich") %>% 
+  group_by(team) %>% 
+  summarise(win = mean(win),
+            rounds = n()) %>% 
+  arrange(rounds %>% desc) 
 
 # Networks ----------------------------------------------------------------
 
@@ -113,32 +132,40 @@ edgelist <- d %>%
 ## making the graph
 # type 0 = debaters, 1 = judges
 g <- graph_from_data_frame(edgelist, directed = F)
-V(g)$type <- ifelse(V(g)$name %in% edgelist$judge == T, 1, 0)
+# V(g)$type <- ifelse(V(g)$name %in% edgelist$judge == T, 1, 0) # this doesn't work for some reason
 
 V(g)$type <- V(g)$name %in% edgelist$judge
 
 plot(g, vertex.label.cex = 1, vertex.color = V(g)$type %>% as.factor, layout = layout.bipartite) # not pretty
 
-walktrap = cluster_walktrap(g)
-louvain = cluster_louvain(g)
-infomap = cluster_infomap(g)
-fast_greedy = cluster_fast_greedy(g)
+cluster_algos <- function(g) {
+  walktrap = cluster_walktrap(g)
+  louvain = cluster_louvain(g)
+  infomap = cluster_infomap(g)
+  fast_greedy = cluster_fast_greedy(g)
+  
+  V(g)$walktrap = walktrap$membership
+  V(g)$louvain = louvain$membership
+  V(g)$infomap = infomap$membership
+  V(g)$fast_greedy = fast_greedy$membership
+  
+  return(g)
+}
 
-V(g)$walktrap = walktrap$membership
-V(g)$louvain = louvain$membership
-V(g)$infomap = infomap$membership
-V(g)$fast_greedy = fast_greedy$membership
 
 ## Tidygraph
 
-t <- g %>% as_tbl_graph()
+tidy_algos <- function(g) {
 
-## tidy node df
-
-cluster_nodes <- t %>% 
-  activate(nodes) %>% 
-  as_tibble() %>%
-  gather("cluster_algorithm", "community", -c(name, type))
+  t <- g %>% as_tbl_graph()
+  
+  cluster_nodes <- t %>% 
+    activate(nodes) %>% 
+    as_tibble() %>%
+    gather("cluster_algorithm", "community", -c(name, type))
+  
+  return(cluster_nodes)
+}
 
 cluster_nodes %>% 
   group_by(cluster_algorithm, community) %>% 
@@ -159,3 +186,45 @@ ggraph(t) +
 #  geom_node_text(aes(label = name), size = .001)
 
 
+# looking at subgraph - only 3+ connections --------------------------------------------------------
+
+el <- edgelist %>% 
+  filter(weight > 2)
+
+e <- graph_from_data_frame(el, directed = FALSE)
+
+V(e)$type <- V(e)$name %in% edgelist$judge
+
+ggraph(e, layout = 'bipartite') + 
+  geom_node_point(aes(color = type), size = .1) +
+  geom_edge_link(aes(width = weight)) +
+  scale_edge_width(range = c(.1, 1))+ # control size
+  geom_node_text(aes(label = name), size = 2) +
+  theme_graph() 
+
+## cluster try2
+
+e2 <- cluster_algos(e)
+
+e3 <- tidy_algos(e2)
+
+e3 %>% 
+  group_by(cluster_algorithm, community) %>% 
+  summarise(count = n())
+
+e3 %>% 
+  group_by(cluster_algorithm) %>% 
+  summarise(count = n_distinct(community))
+
+e3 %>% 
+  arrange(cluster_algorithm, community) %>% 
+  filter(type == TRUE) %>% 
+  #filter(cluster_algorithm == "louvain") %>% 
+  print(n = 300)
+  
+  
+e3 %>% 
+  arrange(cluster_algorithm %>% desc, community) %>% 
+  group_by(cluster_algorithm, community) %>% 
+  summarise(ct = n()) %>% 
+  arrange(ct %>% desc)
